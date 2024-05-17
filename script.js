d9a85ee401383f0ef2b33468d8d848519a472374
vishalhallur <vishalshallur07@gmail.com>
2024-05-17 11:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("hello ");
+console.error("i got error");
+console.warn("warning message");
+//variable
+//let,var,const,
+var number=10
+console.log(number);
+//let is a keyword
+let deptName = "csbs"
+console.log(deptName);
+//const is a keyword
+const lastname="poorni"
+console.log(lastname);
+//undefined
+let poorni;
+console.log(poorni);
+//getelementById() Method
+//It will return the element with the id of "message"
+let message = document.getElementById("msg");
+console.log(message);
+//getelementByclassName() Method
+//It will return the element with the class of "display"
+let display = document.getElementsByClassName("display");
+console.log(display);
+
+
+alert("your account has hacked")
+confirm("tomarrow is holiday ")
